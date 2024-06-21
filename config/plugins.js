a9091912fa77fdf43592f519fbadaead5b8a52dc
tunc alicioglu <tuncalicioglu@tuncs-MacBook-Air.local>
2024-06-21 18:17:01 +0300

    Add necessary configurations for Strapi

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
-    module.exports = ({ env }) => ({
-        'users-permissions': {
-          config: {
-            jwtSecret: env('JWT_SECRET'),
-          },
-        },
-      });
-      
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET'),
+      },
+    },
+  });
+  
