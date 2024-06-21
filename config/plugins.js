a91a1a3fa853e3df40591b62f575590120321bcf
tunc alicioglu <tuncalicioglu@tuncs-MacBook-Air.local>
2024-06-21 17:59:51 +0300

    Add jwtSecret to plugins configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    // Diğer eklenti ayarları buraya gelecek
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET'),
+      },
+    },
+  });
+  
