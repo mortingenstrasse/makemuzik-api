0860c86080fc2ca3aab79100103af458fd018b78
tunc alicioglu <tuncalicioglu@tuncs-MacBook-Air.local>
2024-06-21 23:19:59 +0300

    Serve index.html on root URL

--- config/middleware.js
+++ config/middleware.js
@@ -1,9 +1,40 @@
-module.exports = ({ env }) => ({
-    settings: {
-      cors: {
-        enabled: true,
-        origin: ['http://localhost:8080'],
+module.exports = ({ env }) => [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
       },
     },
-  });
-  
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::router',
+    config: {
+      routes: [
+        {
+          method: 'GET',
+          path: '/',
+          handler: async (ctx) => {
+            ctx.type = 'html';
+            ctx.body = await strapi.fs.readFile(strapi.dirs.public + '/index.html');
+          },
+        },
+      ],
+    },
+  },
+];
