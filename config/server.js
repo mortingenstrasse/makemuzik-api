754d495988b8f1f9316ce344d449d3fb2d7ea17f
tunc alicioglu <tuncalicioglu@tuncs-MacBook-Air.local>
2024-06-22 00:19:51 +0300

    Ensure public folder is served

--- config/server.js
+++ config/server.js
@@ -5,3 +5,20 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
 });
+
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  middleware: {
+    settings: {
+      public: {
+        path: path.resolve(__dirname, '..', 'public'),
+      },
+    },
+  },
+});
