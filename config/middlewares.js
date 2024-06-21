f32b9787c0b801b126cd23ab65ee1e766e396d83
tunc alicioglu <tuncalicioglu@tuncs-MacBook-Air.local>
2024-06-22 00:04:14 +0300

    Update middleware and server configurations

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -8,5 +21,29 @@ module.exports = [
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public',
+  {
+    name: 'strapi::public',
+    config: {
+      path: 'public',
+      maxAge: 60000,
+    },
+  },
+  {
+    name: 'strapi::router',
+    config: {
+      routes: [
+        {
+          method: 'GET',
+          path: '/',
+          handler: async (ctx) => {
+            const fs = require('fs').promises;
+            const path = require('path');
+            const indexPath = path.join(__dirname, '..', 'public', 'index.html');
+            ctx.type = 'html';
+            ctx.body = await fs.readFile(indexPath, 'utf8');
+          },
+        },
+      ],
+    },
+  },
 ];
