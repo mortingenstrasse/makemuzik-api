ad692356983de58b00d5b75ac0ea72b7138caa13
tunc alicioglu <tuncalicioglu@tuncs-MacBook-Air.local>
2024-06-22 00:45:43 +0300

    Add Node.js server to serve index.html

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Static file middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all handler to serve index.html for any routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
