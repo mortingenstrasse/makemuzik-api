b9c07fe69b818704e7ff3ba1ca035d60566d0a9a
tunc alicioglu <tuncalicioglu@tuncs-MacBook-Air.local>
2024-06-22 01:57:57 +0300

    Update server.js and middleware.js for admin panel access

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://www.makemuzik.com'), // Alan adınızı burada belirtiyoruz
+  url: env('PUBLIC_URL', 'https://makemuzik-api-85b50e847530.herokuapp.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
     },
-    url: '/admin', // Admin paneli URL'sini burada belirtiyoruz
+    url: '/admin',
   },
 });
