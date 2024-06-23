65c98cda2fc62eaa2438fc6ac842a573de899e9c
tunc alicioglu <tuncalicioglu@tuncs-MacBook-Air.local>
2024-06-23 19:00:50 +0300

    Update admin.js with new secrets

--- config/admin.js
+++ config/admin.js
@@ -2,5 +2,12 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'Am8NJFgsrPdokWZzgoOehQ=='),
   },
-  url: '/admin',
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'PYVGRdgxtjnVPcmJSSZ+pg=='),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'q9YNNIPjVy2+okHkViu2BWr+2H2jFve5Z40mPzoMurk='),
+    },
+  },
 });
